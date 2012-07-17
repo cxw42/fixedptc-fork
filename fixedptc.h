7f00dd9ddad308a2e1290e586a0728c2cc95eb38
Ivan Voras <ivoras@gmail.com>
2012-07-18 01:30:18 +0200

    Added VCS keyword expansion

--- fixedptc.h
+++ fixedptc.h
@@ -89,7 +89,6 @@ typedef	__uint128_t fixedptud;
 
 #define FIXEDPT_VCSID "$Id$"
 
-
 #define FIXEDPT_FBITS	(FIXEDPT_BITS - FIXEDPT_WBITS)
 #define FIXEDPT_FMASK	(((fixedpt)1 << FIXEDPT_FBITS) - 1)
 
